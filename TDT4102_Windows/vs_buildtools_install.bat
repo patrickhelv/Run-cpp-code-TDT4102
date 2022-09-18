3aa1ddd8e8d5279ca0426442e23f564cf95d7c76
Patrick <113537402+patrickhelv@users.noreply.github.com>
2022-09-18 15:49:09 +0200

    Add files via upload

---
+++ TDT4102_Windows/vs_buildtools_install.bat
@@ -0,0 +1,4 @@
+install\vs_buildtools__603606289.1572297271.exe ^
+--config "%cd%\install\.vsconfig" ^
+--installPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools" ^
+--passive --norestart
